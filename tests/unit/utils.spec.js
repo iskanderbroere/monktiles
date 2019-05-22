a0a2459e6f220aff693bf1e098849c1502b24f55
iskanderbroere <iskanderbroere@gmail.com>
2019-05-22 09:06:53 +0200

    test: add mapindexed test

--- tests/unit/utils.spec.js
+++ tests/unit/utils.spec.js
@@ -1,8 +1,15 @@
-import { square } from '@/utils'
+import { square, mapIndexed } from '@/utils'
 
 describe('square', () => {
   it('squares given numbers', () => {
     expect(square(2)).toBe(4)
     expect(square(3)).toBe(9)
+  })
+})
+
+describe('mapIndexed', () => {
+  it('iterates an array and provides an index in the callback', () => {
+    const arrayOfIndexes = mapIndexed((_, i) => i, Array(3))
+    expect(arrayOfIndexes).toEqual([0, 1, 2])
   })
 })
